41d4d904a141a11490330984efdda406c236f9cf
jayala <Ayala@example.com>
2023-08-13 14:10:37 -0600

    documentacion

--- src/main/java/com/sistema/pedidos/config/MySecurityConfig.java
+++ src/main/java/com/sistema/pedidos/config/MySecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -68,8 +69,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 	}
-	
 
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+				.antMatchers("/swagger-ui/**","/swagger-ui.html", "/v3/api-docs/**");
+	}
 	
 	
 }
