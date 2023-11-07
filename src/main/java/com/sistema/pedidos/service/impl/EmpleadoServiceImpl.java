b865ec3112b266b18e24212227d99cfa6416933e
sishenkira2011 <123423170+sishenkira2011@users.noreply.github.com>
2023-11-06 22:08:52 -0600

    solucion error de listar empleados por paginacion

--- src/main/java/com/sistema/pedidos/service/impl/EmpleadoServiceImpl.java
+++ src/main/java/com/sistema/pedidos/service/impl/EmpleadoServiceImpl.java
@@ -93,7 +93,8 @@ public class EmpleadoServiceImpl extends GenericServiceImpl<Empleado,Integer> im
 
     @Override
     public Page<Empleado> listarEmpleadosPorPagina(Pageable pageable) {
-        return empleadoRepository.findAll(pageable);
+        //return empleadoRepository.findAll(pageable);
+        return empleadoRepository.getAll(pageable);
     }
 
 }
